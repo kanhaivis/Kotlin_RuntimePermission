88b8460e7f2e754da5fda82b4424eea8cbf1ca24
krishan k <kanhaivis@gmail.com>
2020-01-13 23:29:13 +0530

    add toast to all permission is comple

--- app/src/main/java/kk/runtimepermission/MainActivity.kt
+++ app/src/main/java/kk/runtimepermission/MainActivity.kt
@@ -10,6 +10,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.provider.Settings
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -28,9 +29,7 @@ class MainActivity : AppCompatActivity() {
                 // This method will be executed once the timer is over
                 // Start your app main activity
 
-//                val i = Intent(this@MainActivity, WelcomeActivity::class.java)
-//                startActivity(i)
-
+                Toast.makeText(this,"All permission granted!", Toast.LENGTH_LONG).show()
                 // close this activity
                 finish()
             }, SPLASH_TIME_OUT.toLong())
@@ -65,8 +64,7 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         Log.d(TAG, "Permission callback called-------")
         when (requestCode) {
             REQUEST_ID_MULTIPLE_PERMISSIONS -> {
@@ -77,6 +75,7 @@ class MainActivity : AppCompatActivity() {
                 perms[Manifest.permission.WRITE_EXTERNAL_STORAGE] = PackageManager.PERMISSION_GRANTED
                 perms[Manifest.permission.ACCESS_FINE_LOCATION] = PackageManager.PERMISSION_GRANTED
                 perms[Manifest.permission.RECORD_AUDIO] = PackageManager.PERMISSION_GRANTED
+
                 // Fill with actual results from user
                 if (grantResults.size > 0) {
                     for (i in permissions.indices)
@@ -89,19 +88,12 @@ class MainActivity : AppCompatActivity() {
                         Log.d(TAG, "sms & location services permission granted")
                         // process the normal flow
 
-
-
-
-//                        val i = Intent(this@MainActivity, WelcomeActivity::class.java)
-//                        startActivity(i)
-//                        finish()
-
-
-                        //else any one or both the permissions are not granted
+                        Toast.makeText(this,"All permission granted!", Toast.LENGTH_LONG).show()
                     } else {
+
                         Log.d(TAG, "Some permissions are not granted ask again ")
                         //permission is denied (this is the first time, when "never ask again" is not checked) so ask again explaining the usage of permission
-                        //                        // shouldShowRequestPermissionRationale will return true
+                        //shouldShowRequestPermissionRationale will return true
                         //show the dialog or snackbar saying its necessary and try again otherwise proceed with setup.
                         if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)
                                 || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
@@ -118,9 +110,10 @@ class MainActivity : AppCompatActivity() {
                                     })
                         } else {
                             explain("You need to give some mandatory permissions to continue. Do you want to go to app settings?")
-                            //                            //proceed with logic by disabling the related features or quit the app.
+                            //proceed with logic by disabling the related features or quit the app.
                         }//permission is denied (and never ask again is  checked)
                         //shouldShowRequestPermissionRationale will return false
+
                     }
                 }
             }
@@ -140,7 +133,8 @@ class MainActivity : AppCompatActivity() {
     private fun explain(msg: String) {
         val dialog = AlertDialog.Builder(this)
         dialog.setMessage(msg)
-                .setPositiveButton("Yes") { paramDialogInterface, paramInt ->
+                .setPositiveButton("Yes") {
+                    paramDialogInterface, paramInt ->
                     //  permissionsclass.requestPermission(type,code);
                     startActivity(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:com.example.parsaniahardik.kotlin_marshmallowpermission")))
                 }
